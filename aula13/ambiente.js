5a17b68b8e1e447a3399dcf65723c5ecec2a6ea4
Grazzielly Pereira <114615471+ellypereira@users.noreply.github.com>
2023-03-16 15:45:24 -0300

    condição de repetição
    duas maneiras para se repetir mensagens.

---
+++ aula13/ambiente.js
@@ -0,0 +1,30 @@
+/*var c = 1
+while( c <= 5) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1 
+} */
+
+
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ 
+} while (c <= 6)
+
+
+
+
+/* (NÃO É NECESSÁRIO EU FICAR COPIANDO E COLOCANDO MIL VEZES, É SOMENTE EU USAR O CÓDIGO LOGO ABAIXO)
+VAR C = 1
+WHILE( C <= 6){
+CONSOLE.LOG('TUDO BEM!')
+C++ } // CASO EU QUEIRA MUDAR QUANTAS VEZES O NUMERO (TD BEM) SE REPITA É SO ALTERAR ALI NO 6.
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
