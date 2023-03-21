6ce3fa96729f94c757dbd3d361a8807ad01b021e
Grazzielly Pereira <114615471+ellypereira@users.noreply.github.com>
2023-03-21 13:46:42 -0300

    att

---
+++ aula17/objeto01.js
@@ -0,0 +1,14 @@
+/*let amigo = {nome: 'José', 
+sexo: 'M', 
+peso: 85.4, 
+engordar(p=0){
+    console.log('Engordou')
+    this.peso += p
+
+}}
+
+amigo.engordar(2)
+console.log(`${amigo.nome} peso ${amigo.peso}Kg`)*/
+
+let num = 8
+num **=2
