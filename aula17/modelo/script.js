b30f8e20a098738f790c786b01deeb9eb1352532
Grazzielly Pereira <114615471+ellypereira@users.noreply.github.com>
2023-03-21 13:01:04 -0300

    grande dificuldade em fazer esse ex zZ

--- aula17/modelo/script.js
+++ aula17/modelo/script.js
@@ -1,35 +1,39 @@
-let num = document.querySelector('input#fnum')
-let lista= document.querySelector('select#flista')
-let res = document.querySelector('div#res')
-let valores = []
+let numeros = []
 
+function add_numero(){
+    const id_numero = document.querySelector('#numero').value
+    const id_tabela = document.querySelector('#tabela')
 
-function isNumero (n) {
-    if (Number(n) >= 1 && Number(n) <= 100) {
-        return true
-
-    } else {
-        return false
-    }
+    numeros.push(Number(id_numero))
+    id_tabela.innerHTML += `Valor ${id_numero} adicionado.\n`
 }
 
-function inLista(n, 
-    
-) {
-    if (l.indexOf(Number(n)) != -1) {
-        return true
-
+function finalizar(){
+    if (numeros.length == 0){
+        window.alert('[ERRO!] Adicione valores.')
     } else {
-        return false
+        const id_resultado = document.querySelector('#resultado')
+        id_resultado.innerHTML = ''
+
+        const numero_maior = numeros.reduce(function(prev, current) { 
+            return prev > current ? prev : current; 
+        });
+
+        const numero_menor = numeros.reduce(function(prev, current) { 
+            return prev < current ? prev : current; 
+        });
+
+        var soma_valores = 0
+        for (cont in numeros){
+            soma_valores += numeros[cont]
+        }
+
+        id_resultado.innerHTML += `Ao todos temos ${numeros.length} valores.<br>`
+        id_resultado.innerHTML += `O maior número é ${numero_maior}.<br>` 
+        id_resultado.innerHTML += `O menor número é ${numero_menor}.<br>` 
+        id_resultado.innerHTML += `A soma dos valores é ${soma_valores}.<br>` 
+        id_resultado.innerHTML += `A média dos valores é ${soma_valores/numeros.length}`
     }
+ }
 
-}
 
-
-function adicionar() {
-    if(isNumero(num.value) && !inLista(num.value, valores)) {
-
-    } else {
-        window.alert('Valor inválido ou já encontrado na lista.')
-    }
-}
